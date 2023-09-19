952efdc3c74aba1b310fa55261b6608b4f0e4186
AdityaUchiha75 <aditya751999@gmail.com>
2023-09-19 16:44:29 +0200

    Tested out more functions

--- Task 2.1/sample_2D_projections.py
+++ Task 2.1/sample_2D_projections.py
@@ -1,12 +1,16 @@
 import SimpleITK as sitk
 import matplotlib.pyplot as plt
 
-vol_img = sitk.Image()
 
-file_reader = sitk.ImageFileReader()
-file_reader.SetImageIO('NrrdImageIO')
-file_reader.SetFileName('Task 2.1\Data\sample3Dimg.nrrd')
-file_reader.Execute()
+
+
+
+if __name__=="__main__":
+    IMG_PATH=r"C:\Users\Audit\Uppsala - Masters Europe\Semester 3\Project in Image analysis - Software\UCAN-PET-CT-image-data-handling-pipeline\Task 2.1\Data\sample3Dimg.nrrd"
+    vol_img = sitk.ReadImage()
+    file_reader = sitk.ImageFileReader(IMG_PATH)
+    file_reader.SetImageIO('NrrdImageIO')
+    file_reader.SetFileName(IMG_PATH)
 
 # plt.imshow(sitk.GetArrayViewFromImage(logo))
 # plt.axis('off')
