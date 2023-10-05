0d6827a933e500704bb6874cb66b612dbc7c08f2
AdityaUchiha75 <aditya751999@gmail.com>
2023-10-05 13:16:21 +0200

    Updated functions for specific modalities

--- Task 2.1/utils.py
+++ Task 2.1/utils.py
@@ -19,23 +19,28 @@ CT(Grayscale) + PT(Color) = Overlap 2D img
 import SimpleITK as sitk
 import numpy as np
 
-def save_projections(image,img_name,max_intensity=50,min_intensity=-1024):
+def save_projections(image,modality,img_name,max_intensity=50,min_intensity=-1024):
     writer = sitk.ImageFileWriter()
     img=sitk.Extract(image, image.GetSize())
     writer.SetFileName(img_name)
-
+    if modality=="PET" or 'PT':
+        out_min=100.0
+        out_max=255.0
+    else:
+        out_min=0.0
+        out_max=15.0
     #img=make_isotropic(img)
 
     img_write=sitk.Cast(
         sitk.IntensityWindowing(
-            img, windowMinimum=min_intensity, windowMaximum=max_intensity, outputMinimum=100.0, outputMaximum=255.0
+            img, windowMinimum=min_intensity, windowMaximum=max_intensity, outputMinimum=out_min, outputMaximum=out_max
         ),
         sitk.sitkUInt8,
     )
 
     writer.Execute(img_write)  #sitk.Cast(sitk.RescaleIntensity(img,outputMinimum=0,outputMaximum=15)
 
-def get_2D_projections(vol_img,ptype,angle,save_img=True,img_n=''):
+def get_2D_projections(vol_img,modality,type,ptype,angle,save_img=True,img_n=''):
     projection = {'sum': sitk.SumProjection,
                 'mean':  sitk.MeanProjection,
                 'std': sitk.StandardDeviationProjection,
@@ -66,7 +71,21 @@ def get_2D_projections(vol_img,ptype,angle,save_img=True,img_n=''):
     all_points = np.array(all_points)
     min_bounds = all_points.min(0)
     max_bounds = all_points.max(0)
-
+    
+    if modality == 'CT':
+        default_pix_val=0
+        if type == 'Bone' or 'bone' or 'B':
+            vol_img= vol_img > 200
+        elif type == 'Lean Tissue' or 'lean' or 'LT':
+            vol_img= vol_img < 150 and vol_img > -29
+        elif type == 'Adipose' or 'adipose' or 'AT':
+            vol_img= vol_img < -30 and vol_img > -190
+        elif type == 'Air' or 'A':
+            vol_img= vol_img < -191
+    elif modality == 'PET':
+        default_pix_val=-50
+        pass
+    
     #resampling grid will be isotropic so no matter which direction we project to
     #the images we save will always be isotropic (required for vol_img formats that 
     #assume isotropy - jpg,png,tiff...)
@@ -75,11 +94,12 @@ def get_2D_projections(vol_img,ptype,angle,save_img=True,img_n=''):
     new_sz = [int(sz/spc + 0.5) for spc,sz in zip(new_spc, max_bounds-min_bounds)]
 
     pix_array=sitk.GetArrayFromImage(vol_img)
-    maxtensity,mintensity=pix_array.max(),pix_array.min()
+    maxtensity,mintensity=float(pix_array.max()),float(pix_array.min())
     #print(maxtensity,mintensity)
     
     proj_images = []
     i=0
+
     for angle in rotation_angles:
         rotation_transform.SetRotation(rotation_axis, angle) 
         resampled_image = sitk.Resample(image1=vol_img,
@@ -89,14 +109,20 @@ def get_2D_projections(vol_img,ptype,angle,save_img=True,img_n=''):
                                         outputOrigin=min_bounds,
                                         outputSpacing=new_spc,
                                         outputDirection = [1,0,0,0,1,0,0,0,1],
-                                        defaultPixelValue =  -20, #HU unit for air in CT, possibly set to 0 in other cases
+                                        defaultPixelValue = default_pix_val, 
                                         outputPixelType = vol_img.GetPixelID())
         proj_image = projection[ptype](resampled_image, paxis)
         extract_size = list(proj_image.GetSize())
         extract_size[paxis]=0
         proj_images.append(sitk.Extract(proj_image, extract_size))
         if save_img:
-            img_name=img_n + r'_image_{0}.png'.format(i)
-            save_projections(sitk.InvertIntensity(sitk.Extract(proj_image, extract_size),maximum=1),img_name=img_name, max_intensity=maxtensity, min_intensity=mintensity)
+            imgname= img_n + r'_{0}_image_{1}.png'.format(modality + '_' + type,i)
+            save_projections(sitk.InvertIntensity(sitk.Extract(proj_image, extract_size),maximum=1), modality, imgname, max_intensity=maxtensity, min_intensity=mintensity)
             #print(i,angle)
         i+=1
+
+#Testing
+# if __name__=="__main__":
+#     ct_img = sitk.ReadImage(r"C:\Users\Audit\Uppsala - Masters Europe\Semester 3\Project in Image analysis - Software\UCAN-PET-CT-image-data-handling-pipeline\Task 2.1\Data\CTbrain50.nrrd")
+
+#     print()
