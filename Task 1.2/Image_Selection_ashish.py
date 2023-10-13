df0e3eff80e31ec52b7241875bcbae0ee8357c5e
Ashish Chauhan <ashishc.kc@gmail.com>
2023-10-13 22:42:54 +0200

    running multiprocessing version

--- Task 1.2/Image_Selection_ashish.py
+++ Task 1.2/Image_Selection_ashish.py
@@ -1,10 +1,22 @@
 ###### Logic script in order to create a dataframe that contains the exams that fit the selection rules.
 ###### Necessary imports and source path.
 from collections import defaultdict
-import pandas as pd
 import os
 import re
-from distorted import outputDistortedImg
+import sys
+import time
+from datetime import datetime
+import psutil
+import numpy  as np
+import pandas as pd
+import concurrent.futures
+import pydicom as dicom
+import SimpleITK as sitk
+#from distorted import outputDistortedImg
+import random
+random.seed(10)
+
+dicom.config.convert_wrong_length_to_UN = True
 
 source_path = r"G:\ucan_lymfom/"
 ###### Function responsible for displaying the full information of the dataframe
@@ -15,59 +27,82 @@ def display_full(x):
                            "display.max_colwidth", None,
                            ):
         print(x)
-###### Set of rules that affect our exam selection
-### Rules for CT
-CT_ignore_folders = ["bone", "lung", "lunga"]
-CT_specifications_first_set = ["wb", "ax", "venfas"]
-CT_specifications_second_set = ["wb", "ven", "ax"]
-CT_specifications_third_set = ["standard", "ax"]
-CT_specifications_fourth_set = ["standard", "ct", "recon"]
-CT_specifications_fifth_set = ["nat", "ax"]
-CT_specifications_sixth_set = "std"
-CT_specifications_seventh_set = ['venfas', 'ax']
-resolutions = ["3.", "2.", "1."]
 
-### Rules for PET
-PET_specifications_first_set = ["qcfx", "m.free"]
-PET_specifications_second_set = ["vpfx", "m.free"]
-PET_specifications_third_set = "vpfx"
-###### Loading the dataset
-directory_list = list()
-for root, dirs, files in os.walk(source_path, topdown=False):
-    for name in dirs:
-        directory_list.append(os.path.join(root, name))
-###### Creating a dataframe out of the dataset with the required information that are need to proceed with the filtering.
-df = pd.DataFrame(directory_list, columns=['directory'])
-df[['source_directory', 'patient_info']] = df['directory'].str.split(pat='/', n=1, expand=True)
-df[['patient_directory', 'PET-CT_info']] = df['patient_info'].str.split(pat='\\', n=1, expand=True)
-df[['system', 'npr', 'scan_date']] = df['patient_directory'].str.split(pat='_|-', n=2, expand=True)
-temp_df = df.groupby(['npr', 'scan_date']).apply(
-    lambda x: True if x['PET-CT_info'].str.startswith('CT').any() and x['PET-CT_info'].str.startswith(
-        'PT').any() else False).reset_index()
-temp_df = temp_df[temp_df['0' == True]]
-new_df = pd.merge(temp_df, df, how="inner", on=['npr', 'scan_date'], sort=True, suffixes=("_x", "_y"))
-pre_sorted_df = new_df.dropna()
-###### Sort the dataset according to the rules we have in order for the most desired exams to be at the top.
-pre_sorted_df.loc[:, 'Resolutions'] = pre_sorted_df.loc[:, 'PET-CT_info'].str.split('-').str[-1].str.extract(
-    r'(\d+\.\d+)').astype(float)
-pre_sorted_df.loc[:, "Has_QCFX"] = pre_sorted_df.loc[:, "PET-CT_info"].str.contains("QCFX")
-pre_sorted_df.loc[:, "Has_Venfas"] = pre_sorted_df.loc[:, "PET-CT_info"].str.contains("Venfas")
-pre_sorted_df.loc[:, "Has_VEN"] = pre_sorted_df.loc[:, "PET-CT_info"].str.contains("VEN")
-pre_sorted_df.loc[:, "Has_VENFAS"] = pre_sorted_df.loc[:, "PET-CT_info"].str.contains("VENFAS")
-pre_sorted_df.loc[:, "Has_Standard"] = pre_sorted_df.loc[:, "PET-CT_info"].str.contains("Standard")
-pre_sorted_df.loc[:, "Has_Nativ"] = pre_sorted_df.loc[:, "PET-CT_info"].str.contains("Nativ")
-final_df = pre_sorted_df.sort_values(by=['Has_QCFX', 'Has_Venfas', 'Has_VEN', 'Has_VENFAS',
-                                         'Has_Standard', 'Has_Nativ', 'Resolutions'],
-                                     ascending=[False, False, False, False, False, False, False])
-final_df.reset_index(drop=True, inplace=True)
-final_df = final_df.drop(columns=['Has_QCFX', 'Has_Venfas', 'Has_VEN', 'Has_VENFAS',
-                                  'Has_Standard', 'Has_Nativ', 'Resolutions'])
-display_full(final_df['directory'])
-###### Filtering the dataframe and selecting the desired exams for each patient.
-# Create three lists one for CT and PET folders and one for both.
-CT_selected_folders = defaultdict(list)
-PET_selected_folders = defaultdict(list)
-selected_exams = list()
+def checkDistortedImg(vol_img,ptype='mean',angle=90):
+    projection = {'sum': sitk.SumProjection,
+                'mean':  sitk.MeanProjection,
+                'std': sitk.StandardDeviationProjection,
+                'min': sitk.MinimumProjection,
+                'max': sitk.MaximumProjection}
+    paxis = 0
+
+    rotation_axis = [0,0,1]
+    rotation_angles = np.linspace(-1/2*np.pi, 1/2*np.pi, int(180.0/angle)) #15.0 degree 
+
+    rotation_center = vol_img.TransformContinuousIndexToPhysicalPoint([(index-1)/2.0 for index in vol_img.GetSize()])
+
+    rotation_transform = sitk.VersorRigid3DTransform()
+    rotation_transform.SetCenter(rotation_center)
+
+    #Compute bounding box of rotating volume and the resampling grid structure
+    image_indexes = list(zip([0,0,0], [sz-1 for sz in vol_img.GetSize()]))
+    image_bounds = []
+    for i in image_indexes[0]:
+        for j in image_indexes[1]:
+            for k in image_indexes[2]:
+                image_bounds.append(vol_img.TransformIndexToPhysicalPoint([i,j,k]))
+
+    all_points = []
+    for angle in rotation_angles:
+        rotation_transform.SetRotation(rotation_axis, angle)    
+        all_points.extend([rotation_transform.TransformPoint(pnt) for pnt in image_bounds])
+        
+    all_points = np.array(all_points)
+    min_bounds = all_points.min(0)
+    max_bounds = all_points.max(0)
+
+    new_spc = [np.min(vol_img.GetSpacing())]*3
+    new_sz = [int(sz/spc + 0.5) for spc,sz in zip(new_spc, max_bounds-min_bounds)]
+
+    for angle in rotation_angles:
+        rotation_transform.SetRotation(rotation_axis, angle) 
+        resampled_image = sitk.Resample(image1=vol_img,
+                                        size=new_sz,
+                                        transform=rotation_transform,
+                                        interpolator=sitk.sitkLinear,
+                                        outputOrigin=min_bounds,
+                                        outputSpacing=new_spc,
+                                        outputDirection = [1,0,0,0,1,0,0,0,1],
+                                        defaultPixelValue =  -20, #HU unit for air in CT, possibly set to 0 in other cases
+                                        outputPixelType = vol_img.GetPixelID())
+        proj_image = projection[ptype](resampled_image, paxis)
+        extract_size = list(proj_image.GetSize())
+        extract_size[paxis]=0
+        sitk.Extract(proj_image, extract_size)
+
+def outputDistortedImg(df):
+    pid  = os.getpid()
+    ppid = os.getppid()
+    start = time.time()
+    print("PPID %s->%s Started on %s"%(ppid, pid, str(datetime.now())))
+    
+    exception_lst = []
+
+    for _, row in df.iterrows():
+        reader = sitk.ImageSeriesReader()
+        dicom_names = reader.GetGDCMSeriesFileNames(row['directory'])
+        reader.SetFileNames(dicom_names)
+        vol_img = reader.Execute()
+        try:
+            checkDistortedImg(vol_img)
+        except:
+            exception_lst.append(row['directory'])
+
+    end = time.time()
+
+    print("PPID %s Completed in %s"%(os.getpid(), round((end-start)/60,2)))
+
+    return exception_lst
 
 
 def data_filtering(dataframe_column):
@@ -334,37 +369,125 @@ def data_filtering(dataframe_column):
                         selected_exams.append(folder_path)
                         PET_selected_folders[final_part_of_path[0]].append(final_part_of_path[1])
                         continue
-data_filtering(final_df["directory"])
-selected_exams = pd.DataFrame(selected_exams, columns=["directory"])
-print("Number of images: ", selected_exams.shape[0])
 
-selected_exams.to_csv(r"G:\ucan_lymfom/InitialSelected_exams.csv")
-selected_exams.head()
+if __name__ == '__main__':
+
+    ###### Set of rules that affect our exam selection
+    ### Rules for CT
+    CT_ignore_folders = ["bone", "lung", "lunga"]
+    CT_specifications_first_set = ["wb", "ax", "venfas"]
+    CT_specifications_second_set = ["wb", "ven", "ax"]
+    CT_specifications_third_set = ["standard", "ax"]
+    CT_specifications_fourth_set = ["standard", "ct", "recon"]
+    CT_specifications_fifth_set = ["nat", "ax"]
+    CT_specifications_sixth_set = "std"
+    CT_specifications_seventh_set = ['venfas', 'ax']
+    resolutions = ["3.", "2.", "1."]
+
+    ### Rules for PET
+    PET_specifications_first_set = ["qcfx", "m.free"]
+    PET_specifications_second_set = ["vpfx", "m.free"]
+    PET_specifications_third_set = "vpfx"
+    ###### Loading the dataset
+    directory_list = list()
+    for root, dirs, files in os.walk(source_path, topdown=False):
+        for name in dirs:
+            directory_list.append(os.path.join(root, name))
+    ###### Creating a dataframe out of the dataset with the required information that are need to proceed with the filtering.
+    df = pd.DataFrame(directory_list, columns=['directory'])
+    df[['source_directory', 'patient_info']] = df['directory'].str.split(pat='/', n=1, expand=True)
+    df[['patient_directory', 'PET-CT_info']] = df['patient_info'].str.split(pat='\\', n=1, expand=True)
+    df[['system', 'npr', 'scan_date']] = df['patient_directory'].str.split(pat='_|-', n=2, expand=True)
+    temp_df = df.groupby(['npr', 'scan_date']).apply(
+        lambda x: True if x['PET-CT_info'].str.startswith('CT').any() and x['PET-CT_info'].str.startswith(
+            'PT').any() else False).reset_index()
+    temp_df = temp_df[temp_df['0' == True]]
+    new_df = pd.merge(temp_df, df, how="inner", on=['npr', 'scan_date'], sort=True, suffixes=("_x", "_y"))
+    pre_sorted_df = new_df.dropna()
+    ###### Sort the dataset according to the rules we have in order for the most desired exams to be at the top.
+    pre_sorted_df.loc[:, 'Resolutions'] = pre_sorted_df.loc[:, 'PET-CT_info'].str.split('-').str[-1].str.extract(
+        r'(\d+\.\d+)').astype(float)
+    pre_sorted_df.loc[:, "Has_QCFX"] = pre_sorted_df.loc[:, "PET-CT_info"].str.contains("QCFX")
+    pre_sorted_df.loc[:, "Has_Venfas"] = pre_sorted_df.loc[:, "PET-CT_info"].str.contains("Venfas")
+    pre_sorted_df.loc[:, "Has_VEN"] = pre_sorted_df.loc[:, "PET-CT_info"].str.contains("VEN")
+    pre_sorted_df.loc[:, "Has_VENFAS"] = pre_sorted_df.loc[:, "PET-CT_info"].str.contains("VENFAS")
+    pre_sorted_df.loc[:, "Has_Standard"] = pre_sorted_df.loc[:, "PET-CT_info"].str.contains("Standard")
+    pre_sorted_df.loc[:, "Has_Nativ"] = pre_sorted_df.loc[:, "PET-CT_info"].str.contains("Nativ")
+    final_df = pre_sorted_df.sort_values(by=['Has_QCFX', 'Has_Venfas', 'Has_VEN', 'Has_VENFAS',
+                                            'Has_Standard', 'Has_Nativ', 'Resolutions'],
+                                        ascending=[False, False, False, False, False, False, False])
+    final_df.reset_index(drop=True, inplace=True)
+    final_df = final_df.drop(columns=['Has_QCFX', 'Has_Venfas', 'Has_VEN', 'Has_VENFAS',
+                                    'Has_Standard', 'Has_Nativ', 'Resolutions'])
+    display_full(final_df['directory'])
+    ###### Filtering the dataframe and selecting the desired exams for each patient.
+    # Create three lists one for CT and PET folders and one for both.
+    CT_selected_folders = defaultdict(list)
+    PET_selected_folders = defaultdict(list)
+    selected_exams = list()
+
+
+    print("Running the data filtering - initial run")
+    data_filtering(final_df["directory"])
+    selected_exams = pd.DataFrame(selected_exams, columns=["directory"])
+    print("Number of images: ", selected_exams.shape[0])
+
+    logical    = False
+    distorted_lst = []
+    num_procs  = psutil.cpu_count(logical=logical)
+    if len(sys.argv) > 1:
+        num_procs = int(sys.argv[1])
+    print("Number of processes available: ", num_procs)
+
+    print("Splitting dataframe into ", num_procs-1, " dataframes")
+    splitted_df = np.array_split(selected_exams, num_procs-1)
+    start = time.time()
+    with concurrent.futures.ProcessPoolExecutor(max_workers=num_procs) as executor:
+        results = [ executor.submit(outputDistortedImg,df=df) for df in splitted_df ]
+        for result in concurrent.futures.as_completed(results):
+            try:
+                distorted_lst.append(result.result())
+            except Exception as ex:
+                print(str(ex))
+                pass
+    end = time.time()
+
+    print("Writing final distorted images lst")
+    with open(r'F:\U-CAN-Lymfom_A\Selected_for_UCAN_project\DistortedImageTest\distorted_lst.txt', 'w') as fp:
+        for item in distorted_lst:
+            # write each item on a new line
+            fp.write("%s\n" % item)
+        print('Done')
+
+    print("-------------------------------------------")
+    print("PPID %s Completed in %s"%(os.getpid(), round(end-start,2)))
 
 
 
-distorted_lst = outputDistortedImg(selected_exams)
-distorted_lst[:2]
+    #distorted_lst = outputDistortedImg(selected_exams)
+    #distorted_lst[:2]
 
-print("Writing final distorted images directories")
-with open(r'G:\ucan_lymfom\distorted_imagedirs.txt', 'w') as fp:
-    for item in distorted_lst:
-        # write each item on a new line
-        fp.write("%s\n" % item)
-    print('Done')
-#filter distorted images from main dataframe
-final_df1 = final_df[~final_df["directory"].isin(distorted_lst)].copy()
-selected_exams = list() #reset
-data_filtering(final_df1["directory"])
-###### Generate a dataframe with the selected examinations and saving it in the form of an excel file.
-# Sort the dataframe by starting from the newest examination and going to the oldest.
-selected_exams = pd.DataFrame(selected_exams, columns=["directory"])
-selected_exams[["source_directory", "patient_info"]] = selected_exams['directory'].str.split(pat='/', n=1, expand=True)
-selected_exams[['patient_directory', 'PET-CT_info']] = selected_exams['patient_info'].str.split(pat='\\', n=1, expand=True)
-selected_exams.loc[:, 'Date'] = selected_exams['patient_info'].str.split("-").str[1]
-selected_exams = selected_exams.sort_values(by='Date', ascending=False)
-selected_exams.reset_index(drop=True, inplace=True)
-selected_exams = selected_exams.drop(columns='Date')
+    print("Writing final distorted images directories")
+    with open(r'G:\ucan_lymfom\distorted_imagedirs.txt', 'w') as fp:
+        for item in distorted_lst:
+            # write each item on a new line
+            fp.write("%s\n" % item)
+        print('Done')
+    #filter distorted images from main dataframe
+    final_df1 = final_df[~final_df["directory"].isin(distorted_lst)].copy()
+    
+    selected_exams = list() #reset
+    data_filtering(final_df1["directory"])
 
-excel_file_location = r"G:\ucan_lymfom/Selected_exams.xlsx"
-selected_exams.to_excel(excel_file_location)
+    ###### Generate a dataframe with the selected examinations and saving it in the form of an excel file.
+    # Sort the dataframe by starting from the newest examination and going to the oldest.
+    selected_exams = pd.DataFrame(selected_exams, columns=["directory"])
+    selected_exams[["source_directory", "patient_info"]] = selected_exams['directory'].str.split(pat='/', n=1, expand=True)
+    selected_exams[['patient_directory', 'PET-CT_info']] = selected_exams['patient_info'].str.split(pat='\\', n=1, expand=True)
+    selected_exams.loc[:, 'Date'] = selected_exams['patient_info'].str.split("-").str[1]
+    selected_exams = selected_exams.sort_values(by='Date', ascending=False)
+    selected_exams.reset_index(drop=True, inplace=True)
+    selected_exams = selected_exams.drop(columns='Date')
+
+    excel_file_location = r"G:\ucan_lymfom/Selected_exams.xlsx"
+    selected_exams.to_excel(excel_file_location)
