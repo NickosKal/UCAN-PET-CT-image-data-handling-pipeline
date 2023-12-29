8798745e2956f40e9db5b1ac0a2e362b52d1bcaa
Nikolaos Kalampokas <nekalampokas@gmail.com>
2023-12-29 17:50:10 +0100

    updated evaluate models code and created new folder for final results

--- Cross_validation/evaluate_models.py
+++ Cross_validation/evaluate_models.py
@@ -2,9 +2,14 @@ import matplotlib.pyplot as plt
 import os
 import sys
 from tabulate import tabulate
+
 parent_dir = os.path.abspath('../')
+if "UCAN-PET-CT-image-data-handling-pipeline" not in parent_dir:
+    parent_dir = os.path.abspath('./')
+
 if parent_dir not in sys.path:
     sys.path.append(parent_dir)
+print("parent_dir: ", parent_dir)
 
 from Utils import utils
 
@@ -21,14 +26,19 @@ else:
     PATH = ""
     print("Invalid system")
 
-# For regression
-regression_metrics = utils.evaluate_best_models_all_folds(system=2, type="regression", category=None, experiment_number=3, folds_list=list(range(10)))
-print(regression_metrics)
+# For regression: age
+#regression_metrics = utils.evaluate_best_models_all_folds(system=1, type="regression", category=None, experiment_number=3, folds_list=list(range(10)))
+#print(regression_metrics)
 
-# For classification
-sex_classification_metrics = utils.evaluate_best_models_all_folds(system=2, type="classification", category="Sex", experiment_number=2, folds_list=list(range(10)))
+# For classification: sex
+sex_classification_metrics = utils.evaluate_best_models_all_folds(system=1, type="classification", category="Sex", experiment_number=2, folds_list=list(range(10)))
 print(sex_classification_metrics)
 
+# For classification: diagnosis groups - C81, C83, Others
+diagnosis_classification_metrics = utils.evaluate_best_models_all_folds_metric_based(system=1, type="classification", category="Diagnosis", experiment_number=1, folds_list=list(range(10)))
+print(diagnosis_classification_metrics)
+
+# For classification: diagnosis groups - C83.3, C81.1, C81.9
 diagnosis_classification_metrics = utils.evaluate_best_models_all_folds_metric_based(system=1, type="classification", category="Diagnosis", experiment_number=2, folds_list=list(range(10)))
 print(diagnosis_classification_metrics)
 
